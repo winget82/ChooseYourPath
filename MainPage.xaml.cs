a844fda2784e8f6b6c70ad072dab0852b59df239
nfwinget <nflesher82@yahoo.com>
2019-06-30 14:18:29 -0500

    First update

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -24,17 +25,37 @@ namespace ChooseYourPath
     public sealed partial class MainPage : Page
     {
         //Declare Choice object, assign false values
+        Choice userChoice = new Choice(false, false, false, "choice 1", "choice 2", "choice 3");
 
-        Choice userChoice = new Choice(false, false, false, "choice 1", "choice 2", "choice 3", "");
+        //Levels Dictionary
+        Dictionary<string, string[]> levelsDic = new Dictionary<string, string[]>
+            {
+                {"level_001", new string[] {"Text 001", "choice1", "choice2", "choice3"}},//starting level
+                {"level_001a", new string[] {"Text 001a", "choice1a", "choice2a", "choice3a"}},//level as a result of ChoiceButton1
+                {"level_001b", new string[] {"Text 001b", "choice1b", "choice2b", "choice3b"}},//level as a result of ChoiceButton2
+                {"level_001c", new string[] {"Text 001c", "choice1c", "choice2c", "choice3c"}},//level as a result of ChoiceButton3
+                {"level_001aa", new string[] {"Text 001aa", "choice1aa", "choice2aa", "choice3aa"}},
+                {"level_001ab", new string[] {"Text 001ab", "choice1ab", "choice2ab", "choice3ab"}},
+                {"level_001ac", new string[] {"Text 001ac", "choice1ac", "choice2ac", "choice3ac"}}
+        };
+        //to access the values levelsDic["level_001a"][0];
+
+            /*Level scheme will be as follows:
+             * with each choice either an "a", "b", or "c" will be appended to the level name
+             * that appended level name will be the next level loaded
+             * for example level_001abcb would be the level resulting from the following choice order:
+             * ChoiceButton1, ChoiceButton2, ChoiceButton3, and then ChoiceButton2 again
+             * so the path can be traced back in the tree easily
+             */
 
         public MainPage()
         {
             this.InitializeComponent();
 
             bool alive = true;
-            
-            beginGame();
 
+            //beginGame();
+            nextLevel(userChoice.CurrentLevel);
         }
 
         //Class to make an object that contains 3 boolean fields - choice 1, choice 2, and choice 3
@@ -47,9 +68,10 @@ namespace ChooseYourPath
             public string TextValue2 { get; set; }
             public string TextValue3 { get; set; }
             public string LastAnswer { get; set; }
+            public string CurrentLevel { get; set; }
 
             public Choice(bool choice1, bool choice2, bool choice3,
-                string text1, string text2, string text3, string lastAnswer)
+                string text1, string text2, string text3)
             {
                 Choice1 = choice1;
                 Choice2 = choice2;
@@ -57,7 +79,8 @@ namespace ChooseYourPath
                 TextValue1 = text1;
                 TextValue2 = text2;
                 TextValue3 = text3;
-                LastAnswer = lastAnswer;
+                LastAnswer = "";
+                CurrentLevel = "level_001";
             }
         }
 
@@ -67,36 +90,14 @@ namespace ChooseYourPath
             ChoiceButton2.Content = button2Text;
             ChoiceButton3.Content = button3Text;
         }
-
-        public void beginGame()
+        
+        public void nextLevel(string level)
         {
-            levelChoices("Clean your boots", "Stand in the warmth barefooted", "Walk on...");
-            ReadingPane.Text = "You are standing in cow patty...  What would you like to do?";
-            //get method to wait for button click to finish
-
-            level_001();
+            //load next level data from dictionary and update ReadingPane and button text
+            levelChoices(levelsDic[level][1], levelsDic[level][2], levelsDic[level][3]);
+            ReadingPane.Text = levelsDic[level][0];
         }
-
-        public void level_001()
-        {
-            if (userChoice.Choice1)
-            {
-                ReadingPane.Text = "How would you like to clean your boots?";
-                levelChoices("Use a rag", "Wipe them in the grass", "Find a stick");   
-            }
-            else if (userChoice.Choice2)
-            {
-                ReadingPane.Text = "You are standing in cow patty barefoot, man does that feel warm and fresh!" +
-                    "How would you like to proceed?";
-                levelChoices("Walk on...", "Put hands in it...", "Meditate");
-            }
-            else if (userChoice.Choice3)
-            {
-                ReadingPane.Text = "You walk on...  You come to a creek.  What would you like to do?";
-                levelChoices("Fish", "Wash your boots", "Get a drink");
-            }
-        }
-
+        
         private void ChoiceButton1_Click(object sender, RoutedEventArgs e)
         {
             //What happens when ChoiceButton1 is clicked
@@ -105,7 +106,11 @@ namespace ChooseYourPath
             userChoice.Choice2 = false;
             userChoice.Choice3 = false;
             userChoice.LastAnswer = ChoiceButton1.Content.ToString();
-            //something to run next level
+            //update current level
+            string level = userChoice.CurrentLevel.ToString();
+            string updatedLevel = level += "a";
+            userChoice.CurrentLevel = updatedLevel;
+            nextLevel(userChoice.CurrentLevel);
         }
 
         private void ChoiceButton2_Click(object sender, RoutedEventArgs e)
@@ -116,7 +121,11 @@ namespace ChooseYourPath
             userChoice.Choice2 = true;
             userChoice.Choice3 = false;
             userChoice.LastAnswer = ChoiceButton2.Content.ToString();
-            //something to run next level
+            //update current level
+            string level = userChoice.CurrentLevel.ToString();
+            string updatedLevel = level += "b";
+            userChoice.CurrentLevel = updatedLevel;
+            nextLevel(userChoice.CurrentLevel);
         }
 
         private void ChoiceButton3_Click(object sender, RoutedEventArgs e)
@@ -127,7 +136,12 @@ namespace ChooseYourPath
             userChoice.Choice2 = false;
             userChoice.Choice3 = true;
             userChoice.LastAnswer = ChoiceButton1.Content.ToString();
-            //something to run next level
+
+            //update current level
+            string level = userChoice.CurrentLevel.ToString();
+            string updatedLevel = level += "c";
+            userChoice.CurrentLevel = updatedLevel;
+            nextLevel(userChoice.CurrentLevel);
         }
     }
 }
